868c39fd2bf47fd952217a4ce2443a0ba8863376
yogita <veryogita@gmail.com>
2018-11-14 15:47:51 +0530

    select2 added

--- js/app.js
+++ js/app.js
@@ -1,28 +1,10 @@
 $(document).ready(function() {
-    function fail()
-{
-    $(".custom-alert-fail").hide(300);
-}
-$.validator.setDefaults({
-    debug: true,
-    success: "valid"
-  });
-  var form = $( "form" );
-  form.validate();
-  $( ".submitbtn" ).click(function() {
-      if(form.valid())
-    setTimeout(success, 5000);
-    else
-    setTimeout(fail, 5000);
-  });
 
-function success()
-{
-
-    $(".custom-alert-success").hide(300);
-}
-
-
+    $(function () {
+        $('#datetimepicker1').datetimepicker();
+        $('select').select2();
+    });
+          
 
   
 });
